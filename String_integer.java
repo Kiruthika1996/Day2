137fae580bbe979621e2f4639ca1de17e3298af6
Kiruthika1996 <v.ammu79@gmail.com>
2017-06-07 11:22:36 +0530

    Create String_integer.java

---
+++ String_integer.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class String_integer
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+int n=Integer.parseInt(s1);
+System.out.println(n);
+}
+}
